1fb02edb67d3cba45ee3474863c6532086a9865a
Aaron McLeod <sircoolguy99@gmail.com>
2011-09-16 14:31:11 -0400

    added core to dependencies

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,7 @@ Plugin.define do
   version "0.1"
   file    "lib", "html.rb"
   object  "Redcar::Html"
-  dependencies "application",">0",
+  dependencies "core", ">0",
+               "application",">0",
                "edit_view" ,">0"
 end
