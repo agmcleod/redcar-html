d73130cbf01e8da0f4dda1b69dbb4d4ca483f792
Aaron McLeod <sircoolguy99@gmail.com>
2011-09-16 11:55:39 -0400

    wrap command working. added git ignore

--- lib/html.rb
+++ lib/html.rb
@@ -17,10 +17,28 @@ module Redcar
         [map]
     end
     
-    class ConvertToList < Redcar::Command
+    class ConvertToList < Redcar::EditTabCommand
       def execute
-        if doc.selected?
-          
+        if doc.selection?
+          text = doc.selected_text
+          lines = text.split(/\n/)
+          lines.collect! do |c| 
+            wrapped = wrap_text_in_white_space(c) { |t| "<li>#{t}</li>" } 
+            p "Wrapped: |#{wrapped}|"
+            wrapped
+          end
+          doc.replace_selection(lines.join("\n"))
+        end
+      end
+      
+      def wrap_text_in_white_space(text, &wrap_with)
+        r = /^[\t\s]+/
+        if r =~ text
+          partitions = text.partition(r)
+          wrapped = yield(partitions[2])
+          "#{partitions[1]}#{wrapped}"
+        else
+          yield(text)
         end
       end
     end
